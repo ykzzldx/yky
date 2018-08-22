a33e23a0d8cfb64f9b768d7fcd4d989acd58af87
sky <905361022@qq.com>
2018-08-22 11:13:40 +0800

    第二次提交

--- src/test.java
+++ src/test.java
@@ -2,4 +2,9 @@ public class test {
     public static void main(String[] args) {
         System.out.println("xxxxx");
     }
+    public void ss(){
+        int a=1;
+        String s="ss";
+        System.out.println(a+s);
+    }
 }
